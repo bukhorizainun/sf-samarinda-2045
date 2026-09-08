0757309afa6c73036311497743ce7312b22f337f
Mochamad Bukhori Zainun <bukhorizainun@gmail.com>
2026-09-08 17:02:29 +0200

    Terbitkan situs SF

---
+++ _next/static/b6YIdnjzVH5K7ZYI1h5KE/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[lang]","\u002F[lang]\u002Ffutures-lab","\u002F[lang]\u002Fkartu","\u002F[lang]\u002Fkontak","\u002F[lang]\u002Fmini-game","\u002F[lang]\u002Fpermainan","\u002F[lang]\u002Fsamarinda"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
